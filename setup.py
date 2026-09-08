ef8da7c2e905275bcbbfad83aaddcd88718d2055
Bryan Lougheed <bryanlougheed@hotmail.com>
2026-09-08 13:42:43 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='biocal',
-    version='2.0',
+    version='2.0.1',
     description='Using sedimentological priors for more accurate calibration of 14C determinations from bioturbated sediment archives.',
     author='Bryan C. Lougheed',
     author_email='bryan.lougheed@outlook.com',
