7d2689bd218d74375bb7d094d3c8a129b58fc512
Bryan Lougheed <bryanlougheed@hotmail.com>
2026-09-09 13:08:48 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='biocal',
-    version='2.0.1',
+    version='2.0.2',
     description='Using sedimentological priors for more accurate calibration of 14C determinations from bioturbated sediment archives.',
     author='Bryan C. Lougheed',
     author_email='bryan.lougheed@outlook.com',
@@ -10,6 +10,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'numpy',
-        'numba'
+        'numba',
+        'matplotlib'
     ],
 )
